14cd6e66af0d83df3e45e127503bb2ee0750e7f3
Hunter Pollock <hunterpollock2006@gmail.com>
2022-12-17 19:11:43 -0500

    v1.5.1

---
+++ prepblitz/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Permissions;
+using System.Windows.Forms;
+using System.Net;
+using System.IO.Compression;
+
+namespace prepblitz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("C:\\Program Files (x86)\\Blitz3D TSS\\Blitz3D.exe"))
+            {
+                DialogResult result = MessageBox.Show("Blitz3D TSS was not found. Would you like to install Blitz3D TSS? This is required to continue.", "SCPCBR Blitz3D Prepper", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    WebClient web = new WebClient();
+                    Directory.CreateDirectory("C:\\Program Files (x86)\\Blitz3D TSS\\");
+                    web.DownloadFile("https://github.com/HNT8/Blitz3D/releases/download/v1.125/Blitz3D.TSS.zip", "C:\\Program Files (x86)\\Blitz3D TSS\\Blitz3D.zip");
+                    web.Dispose();
+                    ZipFile.ExtractToDirectory("C:\\Program Files (x86)\\Blitz3D TSS\\Blitz3D.zip", "C:\\Program Files (x86)\\Blitz3D TSS\\");
+                } else
+                {
+                    return;
+                }
+            }
+
+            try
+            {
+                foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory() + "\\bin"))
+                {
+                    string filename = Path.GetFileName(file);
+                    if (File.Exists("C:\\Program Files (x86)\\Blitz3D TSS\\bin\\" + filename)) File.Delete("C:\\Program Files (x86)\\Blitz3D TSS\\bin\\" + filename);
+                    File.Copy(file, "C:\\Program Files (x86)\\Blitz3D TSS\\bin\\" + filename);
+                }
+                foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory() + "\\decls"))
+                {
+                    string filename = Path.GetFileName(file);
+                    if (File.Exists("C:\\Program Files (x86)\\Blitz3D TSS\\userlibs\\" + filename)) File.Delete("C:\\Program Files (x86)\\Blitz3D TSS\\userlibs\\" + filename);
+                    File.Copy(file, "C:\\Program Files (x86)\\Blitz3D TSS\\userlibs\\" + filename);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Your Blitz3D TSS installation is broken. Please delete Blitz3D TSS and relaunch this program.", "SCPCBR Blitz3D Prepper");
+                return;
+            }
+
+            MessageBox.Show("Successfully prepped Blitz3D TSS", "SCPCBR Blitz3D Prepper");
+        }
+    }
+}
--- source/Run.bat
+++
@@ -1,8 +0,0 @@
-copy ..\sdk\Release\SCPSDK.dll SCPSDK.dll
-copy ..\sdk\Release\SCPSDK.dll "C:\Program Files (x86)\Blitz3D TSS\bin\SCPSDK.dll"
-copy ..\sdk\SCPSDK.decls "Source Code\SCPSDK.decls"
-copy ..\sdk\SCPSDK.decls "C:\Program Files (x86)\Blitz3D TSS\userlibs\SCPSDK.decls"
-cd Source Code
-4gb_patch.exe ..\SCPCBR.exe
-cd ..\
-start SCPCBR.exe
